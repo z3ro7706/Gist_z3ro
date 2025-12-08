5d6ba44387f7acea5ebda9ec5cb9e06eec261aaa
Z3RO <goldskypark7706@gmail.com>
2025-12-09 02:15:37 +0900

    feat : mcm memoization

--- Code/Algorithm/Practice/MCM_memoization.py
+++ Code/Algorithm/Practice/MCM_memoization.py
@@ -1,8 +1,30 @@
 import sys
 input=sys.stdin.readline
 
-def MCM(arr:list,i:int,j:int):
+def MCM(arr:list,i:int,j:int,table:list):
     if(i==j):
         return 0
     
-    _min=sysmaxsize
+    if(table[i][j]!=-1):
+        return table[i][j]
+
+    _min=sys.maxsize
+
+    for k in range(i,j):
+        count=MCM(arr,i,k,table)+MCM(arr,k+1,j,table)+arr[i-1]*arr[k]*arr[j]
+        if(count<=_min):
+            _min=count
+            table[i][j]=_min
+
+    return _min
+
+n=int(input()) #matrix 개수
+arr=list(map(int,input().split()))
+
+if(len(arr)!=n+1):
+    print("Input data error")
+    exit(0)
+
+
+table=[[-1 for _ in range(0,n+2)]for _ in range(0,n+2)]
+print(MCM(arr,1,n,table))
